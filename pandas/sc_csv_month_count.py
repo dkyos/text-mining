89a4aee7240220ee31937432e2b18f8e73f6f049
dk77.yun <dk77.yun@samsung.com>
2017-09-07 12:05:31 +0900

    update
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- pandas/sc_csv_month_count.py
+++ pandas/sc_csv_month_count.py
@@ -14,6 +14,19 @@ import tensorflow as tf
 logging.basicConfig(level=logging.DEBUG)
 logging.debug('start using PhantomJS')
 
+#np.set_printoptions(threshold=np.inf)
+np.set_printoptions(threshold='nan')
+
+print("==============================")
+print(pd.describe_option('display'))
+print("==============================")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+print("==============================")
+print(pd.describe_option('display'))
+print("==============================")
+
+
 tf.flags.DEFINE_string("csv", "test.csv", "CSV Filename")
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
@@ -32,7 +45,6 @@ df1['date'] = pd.to_datetime(df1['Posting date'], errors='coerce')
 df = pd.DataFrame({'Timestamp': df1.date, 'Category': df1.Category})
 
 df['Month/Year'] = df['Timestamp'].apply(lambda x: "%d/%d" % (x.month, x.year) if not pd.isnull(x) else '')
-np.set_printoptions(threshold=np.inf)
 print (df.groupby(['Month/Year', 'Category']).size())
 #print (df.groupby(['Month/Year', 'Category']).count())
 
