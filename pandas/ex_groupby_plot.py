63176ba4e22e4f7b4487ec92dcfa153df3b22d59
dk77.yun <dk77.yun@samsung.com>
2017-09-09 03:25:50 +0900

    add groupby multiple values, and plotting results example
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

---
+++ pandas/ex_groupby_plot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import csv
+import sys
+import scipy
+import logging
+import numpy as np
+import matplotlib
+import sklearn
+import datetime
+import pandas as pd
+import tensorflow as tf
+import sc_util
+
+sc_util.initialize(logging.INFO)
+
+
+df = pd.read_csv("test.csv", skipinitialspace=True)
+
+print (df)
+print (df.columns)
+
+apple_fplot = df.groupby(['Year','Fungicide'])['Value'].sum()
+
+apple_fplot.columns = ['Year', 'Fungicide', 'Value']
+print (apple_fplot)
+
+matplotlib.style.use('ggplot')
+import matplotlib.pyplot as plt
+
+#plt.figure()
+plot_df = df.groupby(['Year','Fungicide']).sum().unstack()
+#plot_df.plot(subplots=False)
+plot_df.plot(subplots=True)
+plt.show()
+
+#plot_df = apple_fplot.unstack('Fungicide').loc[:, 'Value']
+#plot_df.index = pd.PeriodIndex(plot_df.index.tolist(), freq='A')
+#plot_df.plot()
+
+
+
