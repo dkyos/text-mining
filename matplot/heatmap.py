a608e51eb2a7d58d702965ce95b9b79e43f72816
dk77.yun <dk77.yun@samsung.com>
2017-09-19 22:38:20 +0900

    add heatmap example
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

---
+++ matplot/heatmap.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.random.rand(5, 4)
+heatmap = plt.pcolor(data)
+
+for y in range(data.shape[0]):
+    for x in range(data.shape[1]):
+        plt.text(x + 0.5, y + 0.5, '%.4f' 
+            % data[y, x]
+            , horizontalalignment='center'
+            , verticalalignment='center'
+        )
+
+plt.colorbar(heatmap)
+plt.savefig("foo.png")
