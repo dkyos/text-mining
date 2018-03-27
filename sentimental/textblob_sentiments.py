272543eb92007ce3a0b43dff08064e47392a4dbd
dk77.yun <dk77.yun@samsung.com>
2018-03-27 19:31:04 +0900

    update
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- sentimental/textblob_sentiments.py
+++ sentimental/textblob_sentiments.py
@@ -8,16 +8,21 @@ from textblob.sentiments import NaiveBayesAnalyzer
 # German: "Ich liebe diese Bibliothek"
 # Polish: "Uwielbiam tę bibliotekę"
 
-blob = TextBlob("I love this library", analyzer=NaiveBayesAnalyzer())
-print(blob.sentiment)
+# English: This product does not function properly
+# German: Dieses Produkt funktioniert nicht richtig
+# Polish: Ten produkt nie działa poprawnie
 
-blob = TextBlob("Ich liebe diese Bibliothek", analyzer=NaiveBayesAnalyzer())
-print(blob.detect_language())
-print(blob.translate(to="en"))
-print(blob.sentiment)
 
-blob = TextBlob("Uwielbiam tę bibliotekę", analyzer=NaiveBayesAnalyzer())
-print(blob.detect_language())
-print(blob.translate(to="en"))
-print(blob.sentiment)
+test_string = ["Ich liebe diese Bibliothek"
+    ,"Uwielbiam tę bibliotekę"
+    ,"Dieses Produkt funktioniert nicht richtig"
+    ,"Ten produkt nie działa poprawnie"]
+
+i=0 
+while i<len(test_string): 
+    blob = TextBlob(test_string[i], analyzer=NaiveBayesAnalyzer())
+    print(blob.detect_language())
+    print(blob.translate(to="en"))
+    print(blob.sentiment)
+    i=i+1 
 
